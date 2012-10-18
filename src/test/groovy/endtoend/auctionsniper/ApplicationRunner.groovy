79a4746c052da3a83189f9fdf95de57193f0f1f4
naokirin <naoki.rin186@gmail.com>
2012-10-19 04:18:39 +0900

    Added ApplicationRunner

---
+++ src/test/groovy/endtoend/auctionsniper/ApplicationRunner.groovy
@@ -0,0 +1,37 @@
+package test.groovy.endtoend.auctionsniper
+
+class ApplicationRunner {
+    static final String SNIPER_ID = "sniper"
+    static final String SNIPER_PASSWORD = "sniper"
+    static final String XMPP_HOSTNAME = null
+    static final String STATUS_JOINING = null
+    static final String STATUS_LOST = null
+
+    private AuctionSniperDriver driver
+
+    void startBiddingIn(final FakeAuctionServer auction) {
+        Thread thread = new Thread("Test Application") {
+            @Override void run() {
+                try {
+                    Main.main(XMPP_HOSTNAME, SNIPER_ID, SNIPER_PASSWORD, auction.getItemId())
+                } catch (Exception e) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        thread.setDaemon((true))
+        thread.start()
+        driver = new AuctionSniperDriver(1000)
+        driver.showsSniperStatus(STATUS_JOINING)
+    }
+
+    void showsSniperHasLostAuction() {
+        driver.showsSniperStatus(STATUS_LOST)
+    }
+
+    void stop() {
+        if (driver != null) {
+            driver.dispose()
+        }
+    }
+}
