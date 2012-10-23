a3060b76e9511c0122c8bae2edaa77a60d337dfe
naokirin <naoki.rin186@gmail.com>
2012-10-23 15:05:29 +0900

    Starting with a Test for Bidding

--- src/test/groovy/endtoend/auctionsniper/AuctionSniperEndToEndTest.groovy
+++ src/test/groovy/endtoend/auctionsniper/AuctionSniperEndToEndTest.groovy
@@ -18,6 +18,26 @@ class AuctionSniperEndToEndTest extends Specification {
         notThrown(Exception)
     }
 
+    def "Sniper makes a higher bid but loses"() {
+        given:
+        auction.startSellingItem()
+
+        application.startBiddingIn(auction)
+        auction.hasReceivedJoinRequestFromSniper()
+
+        auction.reportPrice(1000, 98, 'other bidder')
+
+        when:
+        application.hasShownSniperIsBidding()
+        auction.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID)
+
+        auction.announceClosed()
+        application.showsSniperHasLostAuction()
+
+        then:
+        notThrown(Exception)
+    }
+
     def cleanup() {
         auction.stop()
         application.stop()
