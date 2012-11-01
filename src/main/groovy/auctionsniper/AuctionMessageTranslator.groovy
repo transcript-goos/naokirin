1197cf0cef0c0f2ed05022136b47de9849e89bb5
naokirin <naoki.rin186@gmail.com>
2012-11-01 18:31:08 +0900

    Tidying Up the AuctionMessageTranslator

--- src/main/groovy/auctionsniper/AuctionMessageTranslator.groovy
+++ src/main/groovy/auctionsniper/AuctionMessageTranslator.groovy
@@ -13,25 +13,44 @@ class AuctionMessageTranslator implements MessageListener {
 
     @Override
     void processMessage(Chat chat, Message message) {
-        def event = unpackEventFrom(message)
+        def event = AuctionEvent.from(message.body)
+        def eventType = event.type()
 
-        def type = event.get('Event')
-        if ('CLOSE' == type) {
+        if ('CLOSE' == eventType) {
             listener.auctionClosed()
         }
-        else if ('PRICE' == type) {
-            listener.currentPrice(Integer.parseInt(event.get('CurrentPrice')),
-                Integer.parseInt(event.get('Increment')))
+        else if ('PRICE' == eventType) {
+            listener.currentPrice(event.currentPrice(), event.increment())
         }
     }
 
-    private def unpackEventFrom(Message message) {
-        def event = new HashMap()
-        message.body.split(';').each {
-            def pair = it.split(':')
-            event.put(pair[0].trim(), pair[1].trim())
+    private static class AuctionEvent {
+        private final def fields = new HashMap<String, String>()
+        String type() { return get('Event') }
+        int currentPrice() { return getInt('CurrentPrice') }
+        int increment() {return getInt('Increment') }
+
+        private int getInt(String fieldName) {
+            return Integer.parseInt(get(fieldName))
         }
 
-        return event
+        private String get(String fieldName) { return fields.get(fieldName) }
+
+        private void addField(String field) {
+            def pair = field.split(':')
+            fields.put(pair[0].trim(), pair[1].trim())
+        }
+
+        static AuctionEvent from(String messageBody) {
+            def event = new AuctionEvent()
+            fieldsIn(messageBody).each { field ->
+                event.addField(field)
+            }
+            return event
+        }
+
+        static String[] fieldsIn(String messageBody) {
+            return messageBody.split(';')
+        }
     }
 }
